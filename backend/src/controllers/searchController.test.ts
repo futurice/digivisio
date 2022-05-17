92b783a8a1caa908ce97779a6cc63009a8880a56
Verner Fortelius <verner@fortelius.net>
2022-05-17 15:01:03 +0300

    add some basic tests with mocked axios

--- backend/src/controllers/searchController.test.ts
+++ backend/src/controllers/searchController.test.ts
@@ -1,4 +1,12 @@
 import { SearchController } from "./searchController";
+import axios, { AxiosResponse } from 'axios';
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+// todo this is not ideal, waiting for DI
+process.env = Object.assign(process.env, {
+    AOE_API_SEARCH_BASEURL: 'someurl',
+});
 
 describe('searchController', () => {
     describe('getRandom', () => {
@@ -6,6 +14,29 @@ describe('searchController', () => {
             const controller = new SearchController();
             const response = await controller.getRandom()
             expect(response.randomNumber).toBe(9)
+        })
+    })
+
+    describe('postSearch', () => {
+        const searchResponse: AxiosResponse = {
+            data: {
+                someproperty: 'somevalue'
+            },
+            status: 200,
+            statusText: "ok",
+            headers: {},
+            config: {}
+        }
+
+
+        it('should return ok', async () => {
+            mockedAxios.post.mockResolvedValue(searchResponse)
+
+            const controller = new SearchController()
+            const response = await controller.search({})
+
+
+            expect(response).toBeDefined()
         })
     })
 })
