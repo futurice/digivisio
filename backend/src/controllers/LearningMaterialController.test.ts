f7610399e9e2630db2344fe7e4cfdc4f1aaea53e
Antti Ajanki <antti.ajanki@futurice.com>
2022-05-18 16:20:18 +0300

    A simple test for LearningMaterialsController

---
+++ backend/src/controllers/LearningMaterialController.test.ts
@@ -0,0 +1,28 @@
+import { LearningMaterialsController } from "./LearningMaterialsController";
+import axios, { AxiosResponse } from 'axios';
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+describe('learningMaterialsController', () => {
+    describe('get', () => {
+        const searchResponse: AxiosResponse = {
+            data: {
+                someproperty: 'somevalue'
+            },
+            status: 200,
+            statusText: "ok",
+            headers: {},
+            config: {}
+        }
+
+
+        it('should return ok', async () => {
+            mockedAxios.get.mockResolvedValue(searchResponse)
+
+            const controller = new LearningMaterialsController()
+            const response = await controller.getLearningMaterialMetadata("10")
+
+            expect(response).toBeDefined()
+        })
+    })
+})
