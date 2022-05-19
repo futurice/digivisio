a2f6546bb95b2e4e661e1fdbbde6b46f86db854e
Janne Timonen <janne.timonen@futurice.com>
2022-05-19 12:38:59 +0300

    Get learning material metadata on single result page
    * Get learning material metadata on single result page

--- frontend/src/components/ResultPage/ResultPage.tsx
+++ frontend/src/components/ResultPage/ResultPage.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { DefaultService } from '../../utils/apiclient';
 
 const ResultPage = () => {
-  return <div>moi</div>;
+  const { id } = useParams();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [learningMaterial, setLearningMaterial] = useState<any>();
+
+  useEffect(() => {
+    const getResults = async () => {
+      const result = await DefaultService.getLearningMaterialMetadata(id as string);
+      setLearningMaterial(result);
+    };
+    getResults();
+  }, [id, setLearningMaterial]);
+
+  return <div>{JSON.stringify(learningMaterial)}</div>;
 };
 
 export default ResultPage;
