01fc2318ab33656e4079a7d3a5e6f92d76ff1110
Verner Fortelius <verner@fortelius.net>
2022-05-19 13:54:37 +0300

    add basic error handling

--- backend/src/middlewares/authenticationMiddleware.ts
+++ backend/src/middlewares/authenticationMiddleware.ts
@@ -1,5 +1,7 @@
 import { Request } from "express";
 
+export class NotAuthenticatedError extends Error { }
+
 export type AuthenticatedUserModel = {
     userId?: string
     authenticated: boolean
@@ -8,11 +10,12 @@ export type AuthenticatedUserModel = {
 // This is obviously not great authentication here.. but in the real world we would most likely handle authentication outside this application and validate JWTs instead
 export const expressAuthentication = async (request: Request, _securityName: string, _scopes?: string[]): Promise<AuthenticatedUserModel> => {
     const userId = request.headers["digivisio-id"]
+    console.debug(`UserId: ${userId}`);
 
     return userId && typeof userId === 'string'
         ? Promise.resolve({
             authenticated: true,
             userId: userId,
         })
-        : Promise.reject(new Error(`Requests should include 'digivisio-id' header`))    // todo fix this.. use proper response with json and status code 401
+        : Promise.reject(new NotAuthenticatedError(`Requests should include 'digivisio-id' header`))    // todo fix this.. use proper response with json and status code 401
 }
