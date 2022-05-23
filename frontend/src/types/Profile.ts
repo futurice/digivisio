363a180c7c8259b9279aedea7af397082d336304
Carita Niskanen <carita.niskanen@futurice.com>
2022-05-23 16:41:59 +0300

    Add profile images

--- frontend/src/types/Profile.ts
+++ frontend/src/types/Profile.ts
@@ -7,6 +7,7 @@ export type Profile = {
   readonly name: string;
   readonly age: number;
   readonly interest: string;
+  readonly thumbnail: string;
   readonly educationalLevels: ReadonlyArray<SearchFilter>;
   readonly educationalRoles: ReadonlyArray<SearchFilter>;
   readonly fakejwt: string;
