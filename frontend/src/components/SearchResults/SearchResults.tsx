cd3c8116b215f9b253f545cc376eccf283bd56e5
Janne Timonen <janne.timonen@futurice.com>
2022-05-23 14:41:09 +0300

    Improve SearchResults
    * Improve SearchResults style and implementation

--- frontend/src/components/SearchResults/SearchResults.tsx
+++ frontend/src/components/SearchResults/SearchResults.tsx
@@ -2,10 +2,9 @@ import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 import { SearchResponseModel } from '../../utils/apiclient/models/SearchResponseModel';
-import { SearchResult } from '../../utils/apiclient/models/SearchResult';
 import { DefaultService } from '../../utils/apiclient/services/DefaultService';
 import ResultCard from './ResultCard';
-import styles from './Results.module.css';
+import styles from './SearchResults.module.css';
 
 const Results = () => {
   const [searchResultResponse, setSearchResultResponse] = useState<SearchResponseModel>();
@@ -20,23 +19,13 @@ const Results = () => {
     getResults();
   }, [keywordParam]);
 
-  const getResultFields = (result: SearchResult, lang = 'fi') => ({
-    id: result.id,
-    materialName: result.materialName.find((entry) => entry.language === lang)?.materialname,
-    description: result.description.find((entry) => entry.language === lang)?.description,
-  });
-
-  const allResults = searchResultResponse?.results.map((result) => getResultFields(result)) ?? [];
+  const allResults = searchResultResponse?.results ?? [];
 
   return (
     <div className={styles.resultsList}>
-      <div className={styles.resultHits}>
-        <p>Tuloksia:</p>
-        <p>{searchResultResponse?.hits}</p>
-      </div>
       <div className={styles.results}>
-        {allResults.map(({ id, materialName, description }) => (
-          <ResultCard key={id} id={id} materialName={materialName} description={description} />
+        {allResults.map((result) => (
+          <ResultCard key={result.id} result={result} />
         ))}
       </div>
     </div>
