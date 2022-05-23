fd627c2395a310553b8f1aefa0700925a4a4001a
Verner Fortelius <verner@fortelius.net>
2022-05-23 14:58:03 +0300

    add comment

--- backend/src/app.test.ts
+++ backend/src/app.test.ts
@@ -8,7 +8,14 @@ const mockedAxios = axios as jest.Mocked<typeof axios>
 
 const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJha3NlbGkiLCJuYW1lIjoiQWtzZWxpIiwiaWF0IjoxNjUzMDI4MzA4LCJleHAiOjE4NTMwMzE5MDgsImF1ZCI6ImRpZ2l2aXNpbyIsImlzcyI6ImRpZ2l2aXNpb2FwcCJ9.6TKQnmiIs2JdmZcgW-steaTTmIlh2aPycYGTK1z3pNY'
 
-describe('someintegrationtest', () => {
+/**
+ * Basic integration test for doing a search against a mocked api.
+ * Checks application routing (to one endpoint)
+ * Checks connection to db, read/write
+ * Checks that the search writes to the history table.
+ * Checks that the get history endpoint returns the row.
+ */
+describe('post_search_and_get_history', () => {
     describe('postSearch', () => {
         const searchResponseMock: AxiosResponse = {
             data: {
