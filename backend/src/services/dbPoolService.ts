61b4e98eeaa4bd830fe86e37748aae15ebe09824
Verner Fortelius <verner@fortelius.net>
2022-05-24 22:04:22 +0300

    use env variable for postgres host and user

--- backend/src/services/dbPoolService.ts
+++ backend/src/services/dbPoolService.ts
@@ -2,8 +2,8 @@ import { Pool } from 'pg'
 import { getRequiredEnvVariable } from '../utils'
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'postgres',
+    user: getRequiredEnvVariable('POSTGRES_USER'),
+    host: getRequiredEnvVariable('POSTGRES_HOST'),
     database: getRequiredEnvVariable('POSTGRES_DB'),
     password: getRequiredEnvVariable('POSTGRES_PASSWORD'),
     port: 5432,
