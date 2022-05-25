ecffce1d408ac0a63045ac66ed17e2d4bff7c663
Carita Niskanen <carita.niskanen@futurice.com>
2022-05-25 14:04:55 +0300

    Add recommendations for frontpage

--- frontend/src/components/SearchPage/SearchPage.test.tsx
+++ frontend/src/components/SearchPage/SearchPage.test.tsx
@@ -2,13 +2,14 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
+import profiles from '../../data/profiles';
 import SearchPage from './SearchPage';
 
 describe('SearchPage', () => {
   it('should show SearchBar component', () => {
     render(
       <MemoryRouter>
-        <SearchPage learningMode={undefined} setLearningMode={() => null} />
+        <SearchPage selectedProfile={profiles[0]} learningMode={undefined} setLearningMode={() => null} />
       </MemoryRouter>,
     );
     expect(screen.getByLabelText('Hakukenttä koko aineistolle')).toBeInTheDocument();
--- frontend/src/components/SearchPage/SearchPage.tsx
+++ frontend/src/components/SearchPage/SearchPage.tsx
@@ -1,15 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import LearningMode, { LearningModeProps } from '../LearningModeSelector/LearningModeSelector';
+import { LearningMode } from '../../types/LearningMode';
+import { Profile } from '../../types/Profile';
+import { SearchResponseModel } from '../../utils/apiclient/models/SearchResponseModel';
+import { SearchService } from '../../utils/apiclient/services/SearchService';
+import LearningModeSelector from '../LearningModeSelector/LearningModeSelector';
 import SearchBar from '../SearchBar';
+import RecommendationCard from './RecommendationCard';
 import styles from './SearchPage.module.css';
 
-const SearchPage = ({ ...rest }: LearningModeProps) => {
+export type SearchPageProps = {
+  readonly selectedProfile: Profile;
+  readonly learningMode: LearningMode | undefined;
+  readonly setLearningMode: (learningMode: LearningMode) => void;
+};
+
+const SearchPage = ({ ...rest }: SearchPageProps) => {
+  const [searchResultResponse, setSearchResultResponse] = useState<SearchResponseModel>();
+
+  useEffect(() => {
+    setSearchResultResponse(undefined);
+    const getResults = async () => {
+      const results = await SearchService.search({
+        from: 0,
+        size: 4,
+        filters: {
+          educationalLevels: [...rest.selectedProfile.educationalLevels.map(({ key }) => key)],
+          educationalRoles: [...rest.selectedProfile.educationalRoles.map(({ key }) => key)],
+        },
+      });
+      setSearchResultResponse(results);
+    };
+    getResults();
+  }, [rest.selectedProfile.educationalLevels, rest.selectedProfile.educationalRoles]);
+
+  const allResults = searchResultResponse?.results ?? [];
+
   return (
     <>
       <h2 className={styles.title}>Mitä haluat oppia</h2>
-      <LearningMode {...rest} />
+      <LearningModeSelector {...rest} />
       <SearchBar />
+      <div>
+        <h2>Oppijaprofiiliisi liittyvät oppimateriaali suositukset</h2>
+        <div className={styles.results}>
+          {allResults.map((result) => (
+            <RecommendationCard key={result.id} result={result} />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
